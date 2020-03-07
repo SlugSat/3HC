a977ba5e3fedb9cbd3827eaace423f64ee64548f
NJannuzzi <60905303+NJannuzzi@users.noreply.github.com>
2020-03-06 23:15:07 -0800

    Setpoint filtering
    Idiot proofed setpoint input boxes

--- 3HC_UI_Software/GUIrev1.py
+++ 3HC_UI_Software/GUIrev1.py
@@ -6,44 +6,53 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from USB import * 
-from Layout import*
+#from Layout import*
 #from USB.py import *
 
 
 
-
+#($string[0] == '-') ? '-'.ltrim(substr($string, 1), '0') : ltrim($string, '0')
 
 
 #Window Class
 class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
+
+		#test variables
 		self.x = 0
 		self.y = 0
 		self.z = 0
+
+
+		#setpoint values
 		self.XSP = 1
 		self.YSP = 2 
 		self.ZSP = 3
+
+		#flags for invalid setpoints
+		self.xflag = False
+		self.yflag = False
+		self.zflag = False
+
+		#null offsets
+		self.XNOFF = 0
+		self.YNOFF = 0
+		self.ZNOFF = 0
+
+
+
+		#current sensor readings
+		self.XI = 0
+		self.YI = 0
+		self.ZI = 0
+
+
+		#initialize layout of UI
 		self.initUI()
 		
-		#connect USB and verify connection
-		#U = self.connectUSB()
 
 
-	#updates current values dispaly for setpoints	
-	def UpdateSetpoints(self):
-		if(self.XSP):
-			self.XS.setText('X Setpoint (Current Value: %s)' % self.XSP)
-		else:	
-			self.XS.setText('X Setpoint (Current Value: %s)' % 0)
-		if(self.YSP):
-			self.YS.setText('Y Setpoint (Current Value: %s)' % self.YSP)
-		else:	
-			self.YS.setText('Y Setpoint (Current Value: %s)' % 0)
-		if(self.ZSP):
-			self.ZS.setText('Z Setpoint (Current Value: %s)' % self.ZSP)
-		else:	
-			self.ZS.setText('Z Setpoint (Current Value: %s)' % 0)	
 	
 
 
@@ -95,7 +104,7 @@ class MainWindow(QMainWindow):
 
 		#enter X setpoint value
 		self.XSPoint =  QLineEdit(self)
-		self.XSPoint.setValidator(QDoubleValidator(-200, 200, 3))
+		self.XSPoint.setValidator(QDoubleValidator(-200.0, 200.0, 3))
 		self.XSPoint.move(20, 60)
 		self.XSPoint.resize(280,40)
 
@@ -107,7 +116,7 @@ class MainWindow(QMainWindow):
 
 		#enter Y setpoint value
 		self.YSPoint =  QLineEdit(self)
-		self.YSPoint.setValidator(QDoubleValidator(-200, 200, 3))
+		self.YSPoint.setValidator(QDoubleValidator(-200.0, 200.0, 3))
 		self.YSPoint.move(20, 140)
 		self.YSPoint.resize(280,40)
 
@@ -119,7 +128,7 @@ class MainWindow(QMainWindow):
 
 		#enter Z setpoint value
 		self.ZSPoint =  QLineEdit(self)
-		self.ZSPoint.setValidator(QDoubleValidator(-200, 200, 3))
+		self.ZSPoint.setValidator(QDoubleValidator(-200.0, 200.0, 3))
 		self.ZSPoint.move(20, 220)
 		self.ZSPoint.resize(280,40)
 
@@ -138,7 +147,7 @@ class MainWindow(QMainWindow):
 		self.XYZSPButton.clicked.connect(self.SetpointsEntered)
 
 
-		#Calibration labels
+		#Calibration labels (Null Offsets)
 		self.CalLabel = QLabel('Calibration',self)
 		self.CalLabel.move(20,360)
 		self.CalLabel.setFont(QFont('Arial', 20))
@@ -148,56 +157,116 @@ class MainWindow(QMainWindow):
 		self.XNULLLabel = QLabel('X',self)
 		self.XNULLLabel.move(20,420)
 		self.XNULLreadout = QLabel('%d'% 0,self)
-		#self.setCentralWidget(self.XNULLreadout)
 		self.XNULLreadout.move(60,420)
 
 		self.YNULLLabel = QLabel('Y',self)
 		self.YNULLLabel.move(120,420)
 		self.YNULLreadout = QLabel('%d'% 0,self)
-		#self.setCentralWidget(self.YNULLreadout)
 		self.YNULLreadout.move(160,420)
 
 		self.ZNULLLabel = QLabel('Z',self)
 		self.ZNULLLabel.move(220,420)
 		self.ZNULLreadout = QLabel('%d'% 0,self)
-		#self.setCentralWidget(self.ZNULLreadout)
 		self.ZNULLreadout.move(260,420)
 
 
-
+		#field vector graph labels
 		self.FieldVectorLabel = QLabel('Field Vector',self)
 		self.FieldVectorLabel.move(600,20)
 		self.FieldVectorLabel.resize(240,40)
 		self.FieldVectorLabel.setFont(QFont('Arial', 20))
 
-
+		#current sensor readout labels
 		self.CurrentReadoutLabel = QLabel('Current Readouts', self)
 		self.CurrentReadoutLabel.move(600,500)
 		self.CurrentReadoutLabel.resize(240,40)
 		self.CurrentReadoutLabel.setFont(QFont('Arial', 20))
 
 
-
+		#help button
 		self.Helpbutton = QPushButton('WTF is Going On?', self)
 		self.Helpbutton.move(20,800)
 		self.Helpbutton.resize(240,40)
 		self.Helpbutton.clicked.connect(self.NeedHelp)
 
 
-		#update timer
+		#update null offset timer
 		self.NullTimer = QTimer()
-		self.NullTimer.setInterval(1500)
+		self.NullTimer.setInterval(250)
 		self.NullTimer.timeout.connect(self.updatenulloffsets)
 		self.NullTimer.start()
 
 
+		#update current sensors' readings timer
+		self.ITimer = QTimer()
+		self.ITimer.setInterval(2000)
+		self.ITimer.timeout.connect(self.updateIread)
+		self.ITimer.start()
 	#pulls setpoints from text entries and updates setpoint display
 	def SetpointsEntered(self):
-		print("Set Setpoints Clicked")
-		self.XSP = self.XSPoint.text()
-		self.YSP = self.YSPoint.text()
-		self.ZSP = self.ZSPoint.text()
+		#print("Set Setpoints Clicked")
+		# self.XSPoint.setValidator(QDoubleValidator(-200.0, 200.0, 3))
+		# self.YSPoint.setValidator(QDoubleValidator(-200.0, 200.0, 3))
+		# self.ZSPoint.setValidator(QDoubleValidator(-200.0, 200.0, 3))
+		
+		if(self.XSPoint.text() == ""):
+			self.XSP = 0.0
+		else:
+		 	self.XSP = float(self.XSPoint.text())
+		if(self.YSPoint.text() == ""): 
+			self.YSP = 0.0
+		else:
+			self.YSP = float(self.YSPoint.text())
+		if(self.ZSPoint.text() == ""):
+			self.ZSP =0.0
+		else:
+			 self.ZSP = float(self.ZSPoint.text())
+		print(self.XSP)
+		print(self.YSP)
+		print(self.ZSP)
 		self.UpdateSetpoints()
+
+
+
+#updates current values dispaly for setpoints	
+	def UpdateSetpoints(self):
+
+		#makes sure we have a valid input
+		#X
+		if(self.XSP and self.XSPoint.hasAcceptableInput()):
+			self.XS.setText('X Setpoint (Current Value: %s)' % self.XSP)
+		else:
+			if(self.XSP == 0):
+				self.XS.setText('X Setpoint (Current Value: %s)' % 0.0)
+			else:	
+				self.xflag = True
+		#Y
+		if(self.YSP and self.YSPoint.hasAcceptableInput()):	
+			self.YS.setText('Y Setpoint (Current Value: %s)' % self.YSP)
+		else:	
+			if(self.YSP == 0):
+				self.YS.setText('X Setpoint (Current Value: %s)' % 0.0)
+			else:	
+				self.yflag = True
+		#Z
+		if(self.ZSP and self.ZSPoint.hasAcceptableInput()):
+			self.ZS.setText('Z Setpoint (Current Value: %s)' % self.ZSP)
+		else:	
+			if(self.ZSP == 0):
+				self.ZS.setText('X Setpoint (Current Value: %s)' % 0.0)
+			else:	
+				self.zflag = True
+
+			#invalid input popup
+		if(self.xflag or self.yflag or self.zflag):
+			SPInvalid = QMessageBox()
+			SPInvalid.setText('Invalid input(s) for one or more axis setpoints; valid range is -200 to 200 with 3 decimal place maximum precision for each setpoint')
+			self.xflag = False
+			self.yflag = False
+			self.zflag = False
+			SPInvalid.exec_()	
+	
+
 
 	#user pressed help button, bring up HHC manual in new window
 	def NeedHelp(self):
@@ -209,10 +278,10 @@ class MainWindow(QMainWindow):
 
 	#connects and verifies successful USB connection established
 	def connectUSB(self):
-		item = USB()
+		USBitem = USB()
 		#USB_Write()
-		if(item.verify() == True):
-			return item
+		if(USBitem.verify() == True):
+			return USBitem
 		else:	
 			return False
 
@@ -235,13 +304,12 @@ class MainWindow(QMainWindow):
 		#print('Tick Tock')
 
 
+	def updateIread(self):
+		pass
+
 
 #Actually runs the GUI (Main loop)
 if __name__=='__main__':
-
-
-	
-	#This must always come first
 	app = QApplication(sys.argv)
 	mainWindow = MainWindow()
 	mainWindow.show()
